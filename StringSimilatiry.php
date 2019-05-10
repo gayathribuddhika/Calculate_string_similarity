4bb0de6beb6ead5b4b252112ba807bd8060f9953
[Gayathri Buddhika] <[wg.gayathri@gmail.com]>
2019-05-10 11:47:58 +0530

    Calculate_string_similarity in php
    Calculate_string_similarity in php

---
+++ StringSimilatiry.php
@@ -0,0 +1,9 @@
+<?php
+
+$string1 = "This is a sample text.";
+$string2 = "This is an example text of the above topic.";
+
+similar_text($string1,$string2,$result);
+echo "The similarity =  ".$result; 
+
+?>
